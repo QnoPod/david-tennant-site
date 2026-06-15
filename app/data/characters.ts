973b8466bcebb91aacede8a2fec57a138ac9c459
QnoPod <karimailaddress27@gmail.com>
2026-06-15 23:29:58 +0900

    追記6/15

--- app/data/characters.ts
+++ app/data/characters.ts
@@ -1,9 +1,12 @@
 export const customCharacterImages: Record<string, string> = {
   "木曜殺人クラブ": "/characters/Ian Ventham.png",
   "ブロードチャーチ〜殺意の町〜": "/characters/AlecHardy.png",
-  "Staged ステージド": "/characters/michael.jpg",
+  "Staged": "/characters/David.png",
   "80日間世界一周": "/characters/fogg.jpg",
   "Des": "/characters/Des.jpg",
   "Rivals": "/characters/Tony Baddingham.webp",
   "Blackpool": "/characters/Peter Carlisle.png",
+  "ハリー・ポッターと炎のゴブレット": "/characters/Barty Crouch Junior.png",
+  "Inside Man": "/characters/Harry Watling.png",
+  //"": "/characters/",
 };
